33521420799b32f27252186e81cecea61f420499
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-06-15 16:44:08 +0530

    Carousel Update

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,42 @@
 import HeroCarousel from "@/Components/HeroCarousel"
 import Searchbar from "@/Components/SearchBar"
 import Image from "next/image"
-const Home = () => {
-  return (
-    <>
-      <section className="px-6 md:px-20 py-24">
-        <div className="flex max-xl:flex-col gap-16">
-          <div className="flex flex-col justify-center">
-            <p className="small-text">
-              Smart Shopping Starts Here:
-              <Image 
-                src="/assets/icons/arrow-right.svg"
-                alt="Arrow-right"
-                height={16}
-                width={16}
-              />
-            </p>
-            <h1 className="head-text">
-              Unleash the Power of 
-              <span className="text-primary"> PriceTracker</span>
-            </h1>
-            <p className="mt-6">
-              Powerfull, self-serve product and growth analytics to help you convert, engage, and retain more.
-            </p>
+const Home = () => (
+  <>
+    <section className="px-6 md:px-20 py-24">
+      <div className="flex max-xl:flex-col gap-16">
+        <div className="flex flex-col justify-center">
+          <p className="small-text">
+            Smart Shopping Starts Here:
+            <Image
+              src="/assets/icons/arrow-right.svg"
+              alt="Arrow-right"
+              height={16}
+              width={16} />
+          </p>
+          <h1 className="head-text">
+            Unleash the Power of
+            <span className="text-primary"> PriceTracker</span>
+          </h1>
+          <p className="mt-6">
+            Powerfull, self-serve product and growth analytics to help you convert, engage, and retain more.
+          </p>
 
-            {Searchbar}
-          </div>
-          {HeroCarousel}
+          {/* {Searchbar} */}
         </div>
-      </section>
-      <section className="trending-section">
-        <h2 className="section-text">Trending</h2>
+        <HeroCarousel />
+      </div>
+    </section>
+    <section className="trending-section">
+      <h2 className="section-text">Trending</h2>
 
-        <div className="flex flex-wrap gap-x-8 gap-y-16">
-          {['Apple Iphone 15', 'Book', 'Sneakers'].map((product) => (
-            <div>{product}</div>
-          ))}
-        </div>
-      </section>
-    </>
-  )
-}
+      <div className="flex flex-wrap gap-x-8 gap-y-16">
+        {['Apple Iphone 15', 'Book', 'Sneakers'].map((product) => (
+          <div>{product}</div>
+        ))}
+      </div>
+    </section>
+  </>
+)
 
 export default Home
