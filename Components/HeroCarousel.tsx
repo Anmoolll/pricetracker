738b15138b421b7ab7a4da19e360e681f216d15c
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-06-15 16:56:01 +0530

    Absolute path of images

--- Components/HeroCarousel.tsx
+++ Components/HeroCarousel.tsx
@@ -5,11 +5,11 @@ import { Carousel } from 'react-responsive-carousel';
 import Image from "next/image";
 
 const heroImages = [
-  { imgUrl: '/assets/images/hero-1.svg', alt: 'smartwatch'},
-  { imgUrl: '/assets/images/hero-2.svg', alt: 'bag'},
-  { imgUrl: '/assets/images/hero-3.svg', alt: 'lamp'},
-  { imgUrl: '/assets/images/hero-4.svg', alt: 'air fryer'},
-  { imgUrl: '/assets/images/hero-5.svg', alt: 'chair'},
+  { imgUrl: './assets/images/hero-1.svg', alt: 'smartwatch'},
+  { imgUrl: './assets/images/hero-2.svg', alt: 'bag'},
+  { imgUrl: './assets/images/hero-3.svg', alt: 'lamp'},
+  { imgUrl: './assets/images/hero-4.svg', alt: 'air fryer'},
+  { imgUrl: './assets/images/hero-5.svg', alt: 'chair'},
 ]
 
 const HeroCarousel = () => {
