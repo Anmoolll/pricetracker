45c9a20ccc6f1e26ae47bcaa8bec0e58e0e67dce
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-06-16 01:06:41 +0530

    Installed new libraries

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverActions: true,
+        serverComponentsExternalPackages: ['mongoose']
+    },
+    images: {
+        domains: ['m.media-amazon.com']
+    },
     output: "export",
 };
 
-export default nextConfig;
+module.exports = nextConfig;
