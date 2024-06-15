dbfca4d56a8645016cb7cd5aafa2da28bb8dda67
Anmoolll <anmol.agarwal2022@vitstudent.ac.in>
2024-06-16 01:56:08 +0530

    ..

---
+++ lib/scraper/index.ts
@@ -0,0 +1,45 @@
+import axios from 'axios';
+import * as cheerio from 'cheerio'
+import { extractPrice } from '../utils';
+
+export async function scrapeAmazonProduct(url: string){
+    if(!url){
+        return;
+    }
+
+    // curl --proxy brd.superproxy.io:22225 --proxy-user brd-customer-hl_2d3bfced-zone-pricetracker:xu648ummu9ud -k "http://geo.brdtest.com/mygeo.json"
+
+    const username = String(process.env.BRIGHT_DATA_USERNAME);
+    const password = String(process.env.BRIGHT_DATA_PASSWORD);
+    const port = 22225;
+    const session_id = (1000000 * Math.random()) | 0;
+
+    const options = {
+        auth: {
+            username: `${username}-session-${session_id}`,
+            password,
+        },
+        host: 'brd.superproxy.io',
+        port,
+        rejectUnauthorised: false,
+    }
+
+    try {
+        const response = await axios.get(url, options)
+        const $ = cheerio.load(response.data);
+        const title = $('#productTitle').text().trim();
+        const currentPrice = extractPrice(
+            $('.a-section.a-spacing-none.aok-align-center.aok-relative'),
+            $('.aok-offscreen'),
+            $('.priceToPay span.a-price-whole'),
+            $('a.size.base.a-color-price'),
+            $('.a-button-selected .a-color-base'),
+            $('.a-price.a-text-price'),
+            $('.a-price.aok-align-center.reinventPricePriceToPayMargin')
+        );
+        console.log({title, currentPrice});
+        
+    } catch (error: any) {
+        throw new Error(`Failed to Scrape Product: ${error.message}`);
+    }
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,9 @@
 export function extractPrice(...ele: any){
     for(const element of ele){
         const priceText = element.text().trim();
+
+        if(priceText) return priceText.replace(/\D/g, '');
     }
+
+    return '';
 }
